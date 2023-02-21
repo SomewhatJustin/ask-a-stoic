3c82f80b379b2de2cb362d7f717e93946480331e
Justin Fowler <justinfowler1996@gmail.com>
2023-02-21 10:56:07 -0600

    tweaking prompt

--- src/utlis.js
+++ src/utlis.js
@@ -18,7 +18,8 @@ async function askQuestion(phil, question) {
     model: "text-davinci-003",
     prompt: `Respond as the ancient philosopher ${phil} to the following question: ${question}
     Your response should be two paragraphs long. 3-4 sentences per paragraph should be enough. You do not need to provide context for who you are. You can just respond to the question.
-    Then, in a new paragraph, still responding as ${phil}, point the user to the texts or books they can read to learn more about the principles behind your answer.`,
+    Then, in a new paragraph, still responding as ${phil}, point the user to the texts or books they can read to learn more about the principles behind your answer.
+    You should not be referring to ${phil} in the third person. You should be speaking as ${phil} directly to the user.`,
     max_tokens: 300,
     temperature: 0.7,
   });
